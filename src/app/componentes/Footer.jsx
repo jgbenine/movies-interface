4bd70584af13764b26026f3d91b56623c3482d46
João Guilherme <joaobenine@gmail.com>
2023-06-29 20:48:44 -0300

    estrutura footer

--- src/app/componentes/Footer.jsx
+++ src/app/componentes/Footer.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import styles from '../css/Footer.module.scss'
+import { AtSign, Github, Linkedin, MonitorPlay } from 'lucide-react'
+import Link from 'next/link'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className={`bg-gray-900 ${styles.footer}`}>
+      <div className={`bg-gray-900 ${styles.footerWrapper}`}>
+        <p className={styles.description}>
+          Uma plataforma dedicada a fornecer informações sobre filmes.
+          Os usuários podem explorar uma ampla variedade de filmes, desde os mais
+          recentes lançamentos até os clássicos atemporais.
+        </p>
+        <div className={styles.logo}>
+
+        </div>
+        <ul className={styles.footerSocial}>
+          <li>
+            <Link href="" title='Email'>
+              <AtSign
+                size={20}
+                color='#fff'
+              />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://github.com/jaogui/"
+              target='_blank'
+              title='Github'>
+              <Github
+                size={20}
+                color='#fff'
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="" title='Linkedin'>
+              <Linkedin
+                size={20}
+                color='#fff'
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <p className={styles.copyright}>
+        © 2023 · JG · Alguns os direitos reservados.
+      </p>
+    </footer>
   )
 }
 
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className="bg-gray-900 mx-auto">
+      <body className="mx-auto">
         <Header />
         <main>
           {children}
