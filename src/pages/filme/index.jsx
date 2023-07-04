ea1d9660b8e36f45d2b98cbb42e1e2b33428855b
João Guilherme <joaobenine@gmail.com>
2023-07-04 16:50:17 -0300

    estrutura de conteúdo do filme

--- src/pages/filme/index.jsx
+++ src/pages/filme/index.jsx
@@ -2,17 +2,43 @@ import React from 'react'
 import styles from '../../app/css/Filme.module.scss'
 import Header from '@/app/componentes/Header'
 import '../../app/globals.css'
+import Footer from '@/app/componentes/Footer'
 
 function Filme() {
   return (
     <>
-    <Header />
-      <section className={styles.filme}>
+      <Header />
+      <section className={styles.movie}>
         <div className={styles.viewIntro}></div>
-        <div className='container'>
-        teste
-        </div>
+        <article className={`container ${styles.movieContent}`}>
+          <h3>Mad Max: Estrada da Fúria</h3>
+          <ul>
+            <li>
+              2022
+            </li>
+            <li>
+              Tempo
+            </li>
+            <li>
+              Genero
+            </li>
+            <li>
+              Classificação
+            </li>
+            <li>
+              Rating
+            </li>
+          </ul>
+          <p>
+            Em um mundo pós-apocalíptico, Max Rockatansky acredita
+            que a melhor forma de sobreviver é não depender de ninguém.
+            Porém, após ser capturado pelo tirano Immortan Joe e seus rebeldes,
+            Max se vê no meio de uma guerra mortal iniciada pela Imperatriz Furiosa,
+            que tenta salvar um grupo de garotas. Também tentando fugir, Max aceita ajudá-la.
+          </p>
+        </article>
       </section>
+      <Footer />
     </>
   )
 }
