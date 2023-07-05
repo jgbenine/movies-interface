0559ba94ac74793df1be3d755fef7849598a3ffd
João Guilherme <joaobenine@gmail.com>
2023-07-05 16:33:12 -0300

    sessão top da semana

--- src/app/componentes/CartazHorizontal.jsx
+++ src/app/componentes/CartazHorizontal.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import styles from '../css/CartazHorizontal.module.scss'
 import { PlusCircle } from 'lucide-react'
 
-function CartazHorizontal({ infoSerie, ImageHorizontal, altItem }) {
+function CartazHorizontal({ infoSerie, ImageHorizontal, altItem, topFilme }) {
   return (
     <div className={styles.cartazHorizontal}>
       <div className={styles.horizontalImg}>
         <img src={ImageHorizontal} alt={altItem} />
         <span className={styles.filter}>
           <PlusCircle color='#f3f3f3a5' size={30} />
+          <p className={styles.topFilme}>{topFilme}</p>
         </span>
       </div>
       <p className={styles.horizontalInfo}>
--- src/app/componentes/TopSection.jsx
+++ src/app/componentes/TopSection.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import '../../app/globals.css'
 import styles from '../css/TopSection.module.scss'
+import CartazHorizontal from './CartazHorizontal'
 
 function TopSection() {
   return (
     <section className={styles.topSection}>
       <div className={`container ${styles.topGrid}`}>
-        
+        <h3 className={`title-main ${styles.topTitle}`}>Top 5 Filmes da Semana</h3>
+        <div className={styles.topContent}>
+          <CartazHorizontal
+            ImageHorizontal={"/assets/cartaz-img1.jpg"}
+            infoSerie="Joker"
+            topFilme="1"
+          />
+          <CartazHorizontal
+            ImageHorizontal={"/assets/cartaz-img2.jpg"}
+            infoSerie="Pulp Fiction"
+            topFilme="2"
+          />
+          <CartazHorizontal
+            ImageHorizontal={"/assets/cartaz-img12.jpg"}
+            infoSerie="Sniper Americano"
+            topFilme="3"
+          />
+          <CartazHorizontal
+            ImageHorizontal={"/assets/cartaz-img6.jpg"}
+            infoSerie="Interstelar"
+            topFilme="4"
+          />
+          <CartazHorizontal
+            ImageHorizontal={"/assets/cartaz-img7.jpg"}
+            infoSerie="Constantine"
+            topFilme="5"
+          />
+        </div>
       </div>
     </section>
   )
