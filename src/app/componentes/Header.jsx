0eb8397870920548d2392d3562a8ad76a5a5accf
João Guilherme <joaobenine@gmail.com>
2023-07-07 16:59:01 -0300

    menu mobile funfando

--- src/app/componentes/Header.jsx
+++ src/app/componentes/Header.jsx
@@ -6,6 +6,10 @@ import styles from '../css/Header.module.scss'
 function Header() {
   const [menuMobile, setMenuMobile] = React.useState(false);
 
+  function toggleMenuMobile() {
+    setMenuMobile(!menuMobile);
+    console.log('click menu')
+  }
 
   return (
     <header className={styles.header}>
@@ -16,26 +20,28 @@ function Header() {
           </Link>
         </div>
         <nav className={styles.nav}>
-          <ul className={styles.headerNav}>
-            <li>
-              <Play size={15} color='#e3e3e3' />
-              <Link href="/filmes">
-                Filmes
-              </Link>
-            </li>
-            <li>
-              <Play size={15} color='#e3e3e3' />
-              <Link href="/series">
-                Séries
-              </Link>
-            </li>
-            <li>
-              <Play size={15} color='#e3e3e3' />
-              <Link href="/episodios">
-                Episódios
-              </Link>
-            </li>
-          </ul>
+          {menuMobile && (
+            <ul className={styles.headerNav} >
+              <li>
+                <Play size={15} color='#e3e3e3' />
+                <Link href="/filmes">
+                  Filmes
+                </Link>
+              </li>
+              <li>
+                <Play size={15} color='#e3e3e3' />
+                <Link href="/series">
+                  Séries
+                </Link>
+              </li>
+              <li>
+                <Play size={15} color='#e3e3e3' />
+                <Link href="/episodios">
+                  Episódios
+                </Link>
+              </li>
+            </ul>
+          )}
         </nav>
         <div className={styles.headerLinks}>
           <Link href="/login" title='Login'>
@@ -45,7 +51,7 @@ function Header() {
             <UserCircle size={18} />
           </Link>
           <Search size={18} />
-          <button className={styles.btnMobile}>
+          <button className={styles.btnMobile} onClick={toggleMenuMobile}>
             <Menu color='#ffff00' size={21} />
           </button>
         </div>
