77a16c85d1351c764ddda42d92c5c331ee657ec5
João Guilherme <joaobenine@gmail.com>
2023-07-17 11:41:10 -0300

    add filtro image + conteúdo

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,40 +4,47 @@ import "../app/globals.css";
 
 function NotFound() {
   return (
-    <section className="notFound">
-      <section className="sectionTop">
-        <div className="bg-red"></div>
-        <div className="bg-blue"></div>
-        <div className="bg-shell"></div>
-        <div className="bg-purple"></div>
-        <div className="bg-light-blue"></div>
-        <div className="bg-yellow"></div>
-        <div className="bg-green"></div>
+    <>
+      <section className="notFound">
+        <div className="notFound-filter"></div>
+        <section className="sectionTop">
+          <div className="bg-red"></div>
+          <div className="bg-light-blue"></div>
+          <div className="bg-blue"></div>
+          <div className="bg-shell"></div>
+          <div className="bg-purple"></div>
+          <div className="bg-green"></div>
+          <div className="bg-yellow"></div>
+        </section>
+        <section className="sectionMiddle">
+          <div className="bg-blue"></div>
+          <div className="bg-black"></div>
+          <div className="bg-purple"></div>
+          <div className="bg-black"></div>
+          <div className="bg-white"></div>
+          <div className="bg-blue"></div>
+          <div className="bg-black"></div>
+        </section>
+        <section className="sectionBottom">
+          <div className="bg-dark-blue"></div>
+          <div className="bg-blue"></div>
+          <div className="bg-black"></div>
+          <div className="bg-black"></div>
+          <div className="bg-dark-blue"></div>
+          <div className="bg-white"></div>
+        </section>
+        <section className="sectionContent">
+          <div className="message-container">
+            <p className="message">Sem Sinal</p>
+            <p className="message2">
+              Rota não encontrada.. Infelizmente essa rota ainda não está
+              disponível, acesse outra página.
+            </p>
+          </div>
+        </section>
       </section>
-      <section className="sectionMiddle">
-        <div className="bg-blue"></div>
-        <div className="bg-black"></div>
-        <div className="bg-purple"></div>
-        <div className="bg-black"></div>
-        <div className="bg-white"></div>
-        <div className="bg-blue"></div>
-        <div className="bg-black"></div>
-      </section>
-      <section className="sectionBottom">
-        <div className="bg-dark-blue"></div>
-        <div className="bg-blue"></div>
-        <div className="bg-black"></div>
-        <div className="bg-black"></div>
-        <div className="bg-dark-blue"></div>
-        <div className="bg-white"></div>
-      </section>
-      <section className="sectionContent">
-        <div className="message-container">
-          <p className="message">Sem Sinal</p>
-          <p className="message2">Rota não encontrada.. Infelizmente essa rota ainda não está disponível, acesse outra página.</p>
-        </div>
-      </section>
-    </section>
+      <div class="interference"></div>
+    </>
   );
 }
 
