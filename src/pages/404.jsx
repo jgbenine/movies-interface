c3ac604c3047f7334c3cec9ec630ee56d73e1206
João Guilherme <joaobenine@gmail.com>
2023-07-17 10:35:10 -0300

    corrigindo erro css + init 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../app/css/404.scss"
+import '../app/globals.css'
+
+function NotFound() {
+  return (
+    <div className={styles.nFound}>
+
+     textooo
+    </div>
+  );
+}
+
+export default NotFound;
